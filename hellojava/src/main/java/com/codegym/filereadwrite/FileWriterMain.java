7562e0ce4fb94d41220a0675850cc2a219a81145
quangdangcodegym <quang.dang@codegym.vn>
2022-12-28 09:52:18 +0700

    read file

---
+++ hellojava/src/main/java/com/codegym/filereadwrite/FileWriterMain.java
@@ -0,0 +1,89 @@
+package com.codegym.filereadwrite;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileWriterMain {
+    public static void main(String[] args)  {
+        // PrintWriter: 1 luồng kí tự để ghi file
+        List<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Iphone 11", 23000.0));
+        products.add(new Product(2, "Iphone 12", 23000.0));
+
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer(1L, "Quang Dang"));
+        customers.add(new Customer(2, "Thanh Nhan"));
+
+
+        writeDate(products, "./data/product.txt");
+        writeDate(customers, "./data/customer.txt");
+
+
+
+    }
+
+    public static <T> void writeDate(List<T> list, String filePath) {
+        try {
+            File file = new File(filePath);
+
+            PrintWriter printWriter = new PrintWriter(file);
+
+            for (int i = 0; i < list.size(); i++) {
+                printWriter.write(list.get(i).toString());
+                if (i != list.size() - 1) {
+                    printWriter.write("\n");
+                }
+            }
+            printWriter.close();
+
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+    public void docLevel1DungRead() {
+        try {
+            FileReader fileReader = new FileReader("./data/product.txt");
+            String str = "";
+            int cCharacter = -1;
+            while ((cCharacter = fileReader.read()) != -1) {
+                str += (char) cCharacter;
+            }
+            System.out.println(str);
+
+
+        } catch (FileNotFoundException fileNotFoundException) {
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void docLevel2DungBoDemCbuff() {
+        try {
+            FileReader fileReader = new FileReader("./data/product.txt");
+            String str = "";
+
+            char [] cbuff = new char[5];
+            int count = -1;
+            while ((count = fileReader.read(cbuff)) != -1) {
+                for (int i = 0; i < count; i++) {
+                    str += cbuff[i];
+                }
+            }
+            System.out.println(str);
+
+
+        } catch (FileNotFoundException fileNotFoundException) {
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ hellojava/src/main/java/com/codegym/filereadwrite/Product.java
@@ -0,0 +1,64 @@
+package com.codegym.filereadwrite;
+
+import java.util.Objects;
+
+public class Product {
+    protected String name;
+    private int id;
+    private double price;
+
+    public Product(int id,String name,  double price) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Product(){}
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        //1,Iphone 11,23000
+        return String.format("%s,%s,%s", this.id, this.name, this.price);
+    }
+}
